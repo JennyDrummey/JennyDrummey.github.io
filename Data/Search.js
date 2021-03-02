04964c619e52c3b12f6ebcb4ee4abc01d5bcc552
JennyDrummey <jenniferdrummey@boaweb.com>
2021-03-02 11:13:09 -0500

    first commit of help file

---
+++ Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/Data_Insiders_Community.htm'],s:['$','div','newtop','themselv'],p:[['$',5],['also',14],['basic',5],['checkbox',11],['cycle',26],['edit',31],['for',32],['icon',16],['isa',30],['merge',9],['off',31],['popups',28],['result',33],['spaces',5],['that',9],['then',64],['ul',13],['will',68],['¬â€ã¢â',31]]});
---
+++ Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637502803014131845'
+});
